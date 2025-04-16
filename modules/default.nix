cf4bbfa986ba693e6fc626a3676f49f1bf233282
Dhaval Pantojee <dhaval.pantojee@gmail.com>
2025-04-16 22:13:21 +0530

    Added all the packages into home.packages instead of declaring them in individual files.
    - don't want nixos to manage my dotfiles.
    - am working on the dotfiles seperately

--- modules/default.nix
+++ modules/default.nix
@@ -3,11 +3,75 @@
     ...
 }:{
     imports = [
-        ./browser
-        ./editors
-        ./programs
         ./scripts
-        ./hyprland
         ./stylix.nix
+    ];
+
+    home.packages = with pkgs; [
+        firefox
+        neovim
+        vscode
+
+        hyprland
+        hypridle
+        hyprlock
+        waybar
+
+        bat
+        btop
+        jq
+
+        starship
+        zsh
+
+        neofetch
+        fastfetch
+        nnn
+        yazi
+        stow
+
+        zip
+        unzip
+        xz
+        p7zip
+
+        ripgrep
+        jq
+        fzf
+
+        cowsay
+        wl-clipboard
+        file
+        which
+        tree
+        gnused
+        gnutar
+        gawk
+        zstd
+        gnupg
+
+        glow
+        btop
+        iotop
+        iftop
+
+        strace
+        ltrace
+        lsof
+
+        sysstat
+        lm_sensors
+        ethtool
+        pciutils
+        usbutils
+
+        kitty
+        alacritty
+        rofi
+        lazygit
+
+        tmux
+
+        sway
     ];
 }
